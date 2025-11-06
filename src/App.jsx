ad0b4df40eccfe430eb68f2b70f0f6f35f9dd7e3
Amarjit Dey <deyama@Amarjits-MacBook-Air.local>
2025-11-06 21:40:59 +0530

    made some updates to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,41 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+import Write from "./pages/Write";
+import Single from "./pages/Single.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>
+      <Navbar />
+      <Home />
+      <Footer />
+    </div>,
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+]);
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
